37504e09ce24386d5748bf3bd0a5dfa295c87719
Hyunjin Hong <87686021+hyunjin-h@users.noreply.github.com>
2023-11-09 23:08:53 +0900

    Title: 동전 더하기, Time: 129ms, Memory: 24MB, Status: Passed - CodeTree

--- 231109/동전 더하기/add-coins.py
+++ 231109/동전 더하기/add-coins.py
@@ -1,9 +1,12 @@
+#입력
 n,k=map(int,input().split())
 money=[int(input()) for _ in range(n)]
+
+#큰동전부터 다시배열
 money=sorted(money,reverse=True)
 answer=0
 for m in money:
     answer+=k//m
-    if k//m>0:
+    if k//m>0: #굳이 if문 없어도 되네용
         k=k%m
 print(answer)
