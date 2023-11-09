005426be47e9cef26952634898b34d350d553017
Hyunjin Hong <87686021+hyunjin-h@users.noreply.github.com>
2023-11-10 00:32:39 +0900

    Title: 쪼개어 배낭 채우기 구현, Time: 605ms, Memory: 33MB, Status: Passed - CodeTree

---
+++ 231109/쪼개어 배낭 채우기 구현/implement-fractional-knapsack.py
@@ -0,0 +1,19 @@
+#입력
+n,m=map(int,input().split())
+arr=[]
+for _ in range(n):
+    w,v=map(int,input().split())
+    arr.append([v/w,w,v])
+
+arr=sorted(arr, reverse=True)
+bag=m
+ans=0
+for a in arr:
+    if bag>=a[1]:
+        ans+=a[0]*a[1]
+        bag-=a[1]
+    else:
+        ans+=a[0]*bag
+        break
+
+print('{:.3f}'.format(ans))
