1bca54dae4c7dfc89863f1dd2095bbdcfd6cdf68
Hyunjin Hong <87686021+hyunjin-h@users.noreply.github.com>
2023-11-21 17:06:41 +0900

    Title: 아름다운 수, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231121/아름다운 수/beautiful-number.py
+++ 231121/아름다운 수/beautiful-number.py
@@ -4,22 +4,22 @@ result=0
 def check():
     global result
     pre=answer[0]
-    cnt=0
+    cnt=1
     if n==1:
         result=1
         return 0
     for i in range(1,n):
         if pre==answer[i]:
             cnt+=1
-           
         else:
-            pre=answer[i]
-        if cnt//pre==0:
-            
+            cnt=1
+            if i==n-1:
+                return 0         
+        if cnt%pre==0:
             cnt=0
         else:
-
             return 0
+        pre=answer[i]
     result+=1
 
 
@@ -33,8 +33,9 @@ def make(curr_num):
         make(curr_num+1)
         answer.pop()
 
-    print(result)
+
     
     
     
 make(0)
+print(result)
