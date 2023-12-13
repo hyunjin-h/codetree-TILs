c5605234dc2062f550c7115392f3db2e193dc458
Hyunjin Hong <gju08116@hanyang.ac.kr>
2023-12-13 17:28:30 +0900

    Title: 단어 조합, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231213/단어 조합/word-combinations.py
+++ 231213/단어 조합/word-combinations.py
@@ -1,19 +1,30 @@
 n,m=map(int,input().split())
 words=[input() for _ in range(n)]
 
-new='_'.join(words)
-chance=m-len(new)
-new=list(new)
+chance=m-len(''.join(words))-n+1
+
 def push_(chance):
     if chance==0:
         return 
-    l=len(new)
-    for i in range(l-1,-1,-1):
-        if new[i]=='_':
-            new.insert(i,'_')
+
+    for i in range(1,n):
+        if words[i]>'_':
+            words[i]=list(words[i])
+            words[i].insert(0,'_')
+            words[i]=''.join(words[i])
             chance-=1
         if chance==0:
             return
-    push_(chance)        
+    for i in range(n-1,-1,-1):
+        words[i]=list(words[i])
+        words[i].insert(0,'_')
+        words[i]=''.join(words[i])
+        chance-=1
+            
+        if chance==0:
+            return
+    if chance>0:
+        push_(chance)
 push_(chance)
-print(''.join(new))
+
+print('_'.join(words))
