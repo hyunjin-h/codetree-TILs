d3c1e38071153325adefc9e3bbdf43ca17fb556b
Hyunjin Hong <gju08116@hanyang.ac.kr>
2023-12-18 21:21:32 +0900

    Title: 단어 조합, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 231218/단어 조합/word-combinations.py
+++ 231218/단어 조합/word-combinations.py
@@ -1,6 +1,7 @@
+#10100sam님 코드참고
 n, m = map(int, input().split())
 total_length = 0
-blank_length = n-1
+blank_length = n-1  # '_'가 들어갈 자리의 길이
 
 words = []
 for _ in range(n):
@@ -8,7 +9,7 @@ for _ in range(n):
     total_length += len(word)
     words.append(word)
 
-div = (m - total_length) // blank_length
+div = (m - total_length) // blank_length # '_'가 기본적으로 몇개 들어갈지 세팅해주기 위함
 mod = (m - total_length) % blank_length
 
 blanks = [div] * blank_length
@@ -16,13 +17,14 @@ blanks = [div] * blank_length
 for i in range(1, n):
     word = words[i]
     ordinal = ord(word[0])
-    if mod > 0 and ordinal > 90:  # 대문자
-        blanks[i-1] += 1
-        mod -= 1
+    if mod > 0 and ordinal > 90:    # 소문자라면
+        blanks[i-1] += 1            # 앞에 '_'갯수 하나 추가!
+        mod -= 1                    # 남은'_'갯수 업데이트
 
-# 최종 blank 처리
-if max(blanks)-min(blanks)<1:
-    j = len(blanks) - 1
+# 최종 blank 처리 
+# '_의 개수는 모두 같아야 합니다.(1차이까지만 ok)'조건을 만족하기위함
+if max(blanks)-min(blanks)<1:   # 차이가 없을땐 원래대로
+    j = len(blanks) - 1         # 뒤에서부터 하나씩 추가, 사전순으로 앞서는 단어를 만들기 위해
     while mod != 0:
         blanks[j] += 1
         mod -= 1
