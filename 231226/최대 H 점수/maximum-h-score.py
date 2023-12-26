f8b656db8511c49d21f6541e954e2d4fdeb59a62
Hyunjin Hong <gju08116@hanyang.ac.kr>
2023-12-26 23:38:38 +0900

    Title: 최대 H 점수, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231226/최대 H 점수/maximum-h-score.py
+++ 231226/최대 H 점수/maximum-h-score.py
@@ -1,10 +1,17 @@
 n, l = map(int,input().split())
 arr=list(map(int,input().split()))
+
 arr.sort()
 
 h=0
+hi=0
 for i, a in enumerate(arr):
     if len(arr)-i>=a:
         h=max(h,a)
-
+        hi=i
+while l>0:
+    arr[hi]+=1
+    if len(arr)-hi>=arr[hi]:
+        h=arr[hi]
+    l-=1
 print(h)
